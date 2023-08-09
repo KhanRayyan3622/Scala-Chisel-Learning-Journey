bafb3a64e25b21facb9f3767d25e6dc7d79ab1af
KhanRayyan3622 <92469975+KhanRayyan3622@users.noreply.github.com>
2023-08-10 00:37:07 +0500

    Add files via upload

---
+++ src/test/scala/gcd/test_exe3.scala
@@ -0,0 +1,17 @@
+// package Lab_1
+
+// import chisel3._
+// import chisel3.tester._
+// import org.scalatest.FreeSpec
+// import chisel3.experimental.BundleLiterals._
+
+// class Typecast_Tester extends FreeSpec with ChiselScalatestTester
+// {
+//     "Typecast Class" in {
+//         test(new Typecast(8, 5.U)) { a =>
+//             a.clock.step(10)
+//             //a.io.result.expect(true.B)
+
+//         }
+//     }
+// }
