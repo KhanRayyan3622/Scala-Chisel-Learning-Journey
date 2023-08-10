34d01f52dd8779584c36a3481154c6b4abaeb04d
KhanRayyan3622 <92469975+KhanRayyan3622@users.noreply.github.com>
2023-08-10 09:05:11 +0500

    Add files via upload

--- src/main/scala/gcd/Task 1.scala
+++ src/main/scala/gcd/Task 1.scala
@@ -1,24 +1,28 @@
-package Lab_2
-import chisel3 . _
-import chisel3 . util . _
+// package Lab_2
+// import chisel3 . _
+// import chisel3 . util . _
 
-class LM_IO_Interface extends Bundle {
-val s0 = Input ( Bool () )
-val s1 = Input ( Bool () )
-val s2 = Input ( Bool () )
-val out = Output ( UInt (32. W ) )
-}
-class Mux_5to1 extends Module {
-val io = IO (new LM_IO_Interface )
+// class LM_IO_Interface extends Bundle {
+// val s0 = Input ( Bool () )
+// val s1 = Input ( Bool () )
+// val s2 = Input ( Bool () )
+// val out = Output ( UInt (32. W ) )
+// }
+// class Mux_5to1 extends Module {
+// val io = IO (new LM_IO_Interface )
 
-// Start coding here
-val sel=Cat(io.s2,io.s1,io.s0)
-io . out := MuxCase ( false .B , Array (
-    (sel === "b000".U) -> 0.U,
-    (sel === "b001".U) -> 8.U,
-    (sel === "b010".U) -> 16.U,
-    (sel === "b011".U) -> 24.U,
-    (sel === "b100".U) -> 32.U
-))
+// // Start coding here
+// //  the Cat function is used to concatenate the three Boolean signals io.s2, io.s1, and io.s0 into a single signal called sel. 
+// val sel=Cat(io.s2,io.s1,io.s0)
+// // false.B: This is the default value that will be used if none of the case conditions match. In this case, 
+// // it's a Boolean constant false.
+// // array(: This is the beginning of an array that will hold the case conditions and values for the multiplexer.
+// io . out := MuxCase ( false .B , Array (
+//     (sel === "b000".U) -> 0.U,  //If sel is "000", the output is 0.
+//     (sel === "b001".U) -> 8.U,  //If sel is "001", the output is 8.
+//     (sel === "b010".U) -> 16.U,
+//     (sel === "b011".U) -> 24.U,
+//     (sel === "b100".U) -> 32.U  //If sel is "100", the output is 32
+// ))
 
-}
+// }
