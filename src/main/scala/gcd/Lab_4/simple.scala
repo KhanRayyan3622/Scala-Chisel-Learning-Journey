4b51ec3864dfc36111eb54c1fa28e7a6bc94c4c5
KhanRayyan3622 <92469975+KhanRayyan3622@users.noreply.github.com>
2023-10-03 01:30:45 +0500

    Create simple.scala

---
+++ src/main/scala/gcd/Lab_4/simple.scala
@@ -0,0 +1,12 @@
+// package Lab_4
+// import chisel3._
+// import chisel3.util._
+// //import chisel3 . iotesters .{ Driver , PeekPokeTester }
+
+// class Simple extends Module {
+//     val io = IO(new Bundle {
+//         val in = Input(UInt(16.W))
+//         val out = Output(UInt(16.W))
+//     })
+//     io.out := log2Ceil(io.in)
+// }
