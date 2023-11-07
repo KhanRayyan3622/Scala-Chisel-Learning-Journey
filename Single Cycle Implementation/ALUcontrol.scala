33b7be0c72c61d6f8e909056a758ac6f7836d0f3
Rayyan Khan <khan_rayyan22sept@outlook.com>
2023-11-07 09:58:45 +0500

    Update ALUcontrol.scala

--- Single Cycle Implementation/ALUcontrol.scala
+++ Single Cycle Implementation/ALUcontrol.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 
 class Alu_Control_IO(width: Int, f3: UInt) extends Bundle {
 
-	val Aluop = Input(UInt(width.W))
+	val aluop = Input(UInt(width.W))
 	val func7 = Input(Bool())
 	val func3 = Input(f3)
 	val control = Output(UInt(5.W))
@@ -17,26 +17,26 @@ class AluControl extends Module with Config{
 	io.control := MuxCase("b000".U, Array(
 
 		// R Type 
-		(io.Aluop === "b000".U && io.func7 === false.B) -> Cat("b0".U, io.func3),
-		(io.Aluop === "b000".U && io.func7 === true.B) -> Cat("b1".U, io.func3),
+		(io.aluop === "b000".U && io.func7 === false.B) -> Cat("b0".U, io.func3),
+		(io.aluop === "b000".U && io.func7 === true.B) -> Cat("b1".U, io.func3),
 
 		// Srli
-		(io.Aluop === "b001".U && io.func3 === "b101".U && io.func7 === false.B) -> Cat("b0".U , io.func3),
+		(io.aluop === "b001".U && io.func3 === "b101".U && io.func7 === false.B) -> Cat("b0".U , io.func3),
 
 		//Srai
-		(io.Aluop === "b001".U && io.func3 === "b101".U && io.func7 === true.B) -> Cat("b1".U , io.func3),
+		(io.aluop === "b001".U && io.func3 === "b101".U && io.func7 === true.B) -> Cat("b1".U , io.func3),
 
 		//SLLI
-		(io.Aluop === "b001".U && io.func3 === "b001".U) -> Cat("b0".U , io.func3),
+		(io.aluop === "b001".U && io.func3 === "b001".U) -> Cat("b0".U , io.func3),
 
 		//All Remaining I-Type
-		(io.Aluop === "b001".U) -> Cat("b0".U , io.func3),
+		(io.aluop === "b001".U) -> Cat("b0".U , io.func3),
 
 		// Branch
-		(io.Aluop === "b010".U) -> Cat("b010".U , io.func3),
+		(io.aluop === "b010".U) -> Cat("b010".U , io.func3),
 
 		//JALR , JAL
-		(io.Aluop === "b011".U) -> "b11111".U  
+		(io.aluop === "b011".U) -> "b11111".U  
 
 		))
 }
